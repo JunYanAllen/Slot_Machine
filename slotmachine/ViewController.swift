60945277ac4c493b105ec82b68d1204f1ac98250
zhangjunyan <olk022150663@gmail.com>
2019-05-20 10:41:45 +0800

    Initial Commit

---
+++ slotmachine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  slotmachine
+//
+//  Created by 張駿彥 on 2019/5/20.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
